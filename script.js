63fafcda9038cb2a8750dd8321200c0ec01edd73
VarvaraSorokina <99732956+VarvaraSorokina@users.noreply.github.com>
2023-01-13 16:30:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+// Задача 1
+//function buttonClick(){
+//   let elem = document.getElementById('elem');
+//    elem.innerHTML = 'Ку-ку! А я <b>жирный</b>!';
+// }
+
+// Задача 2
+//function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.outerHTML = '<h3><b>Абзац превратился в h3!</b></h3>';
+// }
+
+// Задча 3
+//function buttonClick(){
+//let elem = document.getElementById('elem');
+//    elem.innerHTML = '<b>При нажатии на кнопку абзац станет h3, но текст останется.</b>';
+//}
+
+// Задача 5
+//function buttonClick(){
+//         let elems = document.getElementsByTagName('p');
+//         for(let i = 0; i<elems.length; i++){
+//             elems[i].innerHTML = i+0;
+//        }
+//}
+
+
+
+
+
+
+// Задача 4
+//let elem = document.getElementById('test');
+//elem.setAttribute('value', '33');
+//console.log(elem.getAttribute('value'));
+
+//function sum (x){
+//    var s = 0;
+//    for (i = 0; i < x.length; i++){
+//       s += x[i]
+//    }
+//    return s
+// }
